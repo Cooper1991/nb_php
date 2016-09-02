2c3c813091278e646a1b13d86713ddc5cf9d52a5
Jack Cooper <JCooper@GRAPEVINE.local>
2016-09-02 17:23:56 +0100

    find and replace app

---
+++ find_replace_app/index.php
@@ -0,0 +1,35 @@
+<?php
+
+if(isset($_POST['text']) && ($_POST['searchfor']) && ($_POST['replacewith'])){
+	
+	$text = $_POST['text'];
+	$search = $_POST['searchfor'];
+	$replace = $_POST['replacewith'];
+	
+	if(!empty($text) && !empty($search) && !empty($replace)){
+		
+		echo 'Okay';
+		
+	} else {
+		
+		echo 'Please fill in all fields';
+		
+	}
+	
+}
+
+
+
+?>
+
+
+<form action="index.php" method="POST">
+
+	<textarea name="text" rows="6" col="30"></textarea> <br><br>
+	Search: <br>
+	<input type="text" name="searchfor"> <br><br>
+	Replace with: <br>
+	<input type="text" name="replacewith"> <br><br>
+	<input type="submit" value="Find and Replace">
+
+</form>
